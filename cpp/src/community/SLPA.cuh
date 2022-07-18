016d31597e3fcca03b06e62d3f8346eaec92c805
sanilnvidia <sanilr@nvidia.com>
2022-07-18 17:09:56 -0400

    updated baseline SLPA need to move to cugraph graph

--- cpp/src/community/SLPA.cuh
+++ cpp/src/community/SLPA.cuh
@@ -41,7 +41,6 @@
 namespace cugraph {
 //specfic functors for SLPA
 namespace {
-
 __host__ __device__
 unsigned int hash(unsigned int a)
 {
@@ -54,7 +53,6 @@ unsigned int hash(unsigned int a)
     return a;
 }
 
-
 struct get_label 
 {
     int * col;
@@ -62,10 +60,10 @@ struct get_label
     int * memnnz;
     int n;
     //int * _col, int * _mem, int * _memnnz,
-    get_label(int _n, thrust::device_vector<int> _col, thrust::device_vector<thrust::tuple<int,int> > _mem, thrust::device_vector<int> _memnnz) {
-        col= thrust::raw_pointer_cast(&_col[0]);
-        mem = thrust::raw_pointer_cast(&_mem[0]);
-        memnnz= thrust::raw_pointer_cast(&_memnnz[0]);
+    get_label(int _n, thrust::universal_vector<int> &_col, thrust::universal_vector<thrust::tuple<int,int> > &_mem, thrust::universal_vector<int> &_memnnz) {
+        col= thrust::raw_pointer_cast(_col.data());
+        mem = thrust::raw_pointer_cast(_mem.data());
+        memnnz= thrust::raw_pointer_cast(_memnnz.data());
         //labellist = thrust::raw_pointer_cast(&_labellist[0]);
         n = _n;
 
@@ -73,35 +71,18 @@ struct get_label
   __device__
   thrust::tuple<int,int> operator()(unsigned int thread_id)
   {
-    unsigned int seed = hash(thread_id);
-    if(thread_id == 0) {
-        printf("this is n %d\n this is col[0] %d\n", n, col[0]);
-        printf("this is mem[0] %d\n", thrust::get<0>(mem[5]));
-        printf("this is memnnz[0] %d\n", memnnz[2]);
+    int max = 0;
+    int offset = -1;
+    for(int i = 0; i < memnnz[col[i]]; i++){
+      if(thrust::get<1>(mem[col[thread_id] * n + i]) > max) {
+        offset = i;
+        max = thrust::get<1>(mem[col[thread_id] * n + i]);
+      }
     }
-    // seed a random number generator
-    thrust::default_random_engine rng(seed);
-
-    // create a mapping from random numbers to [0,1)
-    thrust::uniform_real_distribution<float> u01(0,1);
-    if(thread_id == 0) {
-        printf("This is the u01 %f\n", u01(rng));
-        printf("this is memnnz col thread %d\n", memnnz[col[thread_id]]);
-        printf("THis is the range u01 %f\n", u01(rng)*2.0);
-        printf("this is the offset %d\n", (int)round(u01(rng)*memnnz[col[thread_id]]));
-    }
-      // draw a sample from the unit square
-    int offset = (int)truncf(u01(rng)*memnnz[col[thread_id]]);
-    
-    // printf("%d\n", mem[col[thread_id] * n + offset]);
-    // labellist[thread_id] = mem[col[thread_id] * n + offset];
-    // printf("%d\n", labellist[thread_id]);
-    // return labellist[thread_id];
     thrust::tuple<int, int> val(thrust::get<0>(mem[col[thread_id] * n + offset]), 1);
-    return mem[col[thread_id] * n + offset];
+    return val;
   }
 };
-
 
 struct get_frequency 
 {
@@ -111,11 +92,11 @@ struct get_frequency
     thrust::tuple<int,int> * labellist;
     int n;
     //int * _col, int * _mem, int * _memnnz,
-    get_frequency(int _n, thrust::device_vector<int> _row, thrust::device_vector<thrust::tuple<int,int> > _mem, thrust::device_vector<thrust::tuple<int,int> > _labellist, thrust::device_vector<int> _row_id) {
-        row= thrust::raw_pointer_cast(&_row[0]);
-        mem = thrust::raw_pointer_cast(&_mem[0]);
-        labellist = thrust::raw_pointer_cast(&_labellist[0]);
-        row_id = thrust::raw_pointer_cast(&_row_id[0]);
+    get_frequency(int _n, thrust::universal_vector<int> &_row, thrust::universal_vector<thrust::tuple<int,int> > &_mem, thrust::universal_vector<thrust::tuple<int,int> > &_labellist, thrust::universal_vector<int> &_row_id) {
+        row= thrust::raw_pointer_cast(_row.data());
+        mem = thrust::raw_pointer_cast(_mem.data());
+        labellist = thrust::raw_pointer_cast(_labellist.data());
+        row_id = thrust::raw_pointer_cast(_row_id.data());
         n = _n;
     }
   __device__
@@ -128,18 +109,146 @@ struct get_frequency
       if(thrust::get<0>(labellist[thread_id]) == thrust::get<0>(labellist[i]))
         count++;
     }
-    thrust::get<1>(labellist[thread_id]) = count;
-    return labellist[thread_id];
+    thrust::tuple<int,int> val(thrust::get<0>(labellist[thread_id]), count);
+    return val;
   }
 };
-template <typename edge_t>
-struct reduce_per_offset
+
+struct sort_per_row 
 {
-    __device__ void operator()(labellist) const 
-    { 
-        //get column offsets from graph
-        //use those to go over specific labellist range and find frequency
+    int * row_id;
+    int * row;
+    int * col;
+    thrust::tuple<int,int> * labellist;
+    int n;
+    //int * _col, int * _mem, int * _memnnz,
+    sort_per_row(int _n,thrust::universal_vector<int> &_row, thrust::universal_vector<int> &_col, thrust::universal_vector<thrust::tuple<int,int> > &_labellist, thrust::universal_vector<int> &_row_id) {
+        row= thrust::raw_pointer_cast(_row.data());
+        col= thrust::raw_pointer_cast(_col.data());
+        labellist = thrust::raw_pointer_cast(_labellist.data());
+        row_id = thrust::raw_pointer_cast(_row_id.data());
+        n = _n;
     }
+  __device__
+  void operator()(unsigned int thread_id)
+  {
+    if(thrust::get<1>(labellist[row[row_id[thread_id]]]) < thrust::get<1>(labellist[thread_id])) {
+      atomicCAS(&(thrust::get<0>(labellist[row[row_id[thread_id]]])), thrust::get<0>(labellist[row[row_id[thread_id]]]), thrust::get<0>(labellist[thread_id]));
+      atomicCAS(&(thrust::get<1>(labellist[row[row_id[thread_id]]])), thrust::get<1>(labellist[row[row_id[thread_id]]]), thrust::get<1>(labellist[thread_id]));
+    }
+  }
+};
+
+
+struct check_ties 
+{
+    int * row_id;
+    int * labellistnnz;
+    int * row;
+    int * col;
+    thrust::tuple<int,int> * labellist;
+    int n;
+    //int * _col, int * _mem, int * _memnnz,
+    check_ties(int _n,thrust::universal_vector<int> &_row, thrust::universal_vector<int> &_col, thrust::universal_vector<thrust::tuple<int,int> > &_labellist, thrust::universal_vector<int> &_row_id,
+    thrust::universal_vector<int> &_labellistnnz) {
+        row= thrust::raw_pointer_cast(_row.data());
+        col= thrust::raw_pointer_cast(_col.data());
+        labellist = thrust::raw_pointer_cast(_labellist.data());
+        row_id = thrust::raw_pointer_cast(_row_id.data());
+        labellistnnz = thrust::raw_pointer_cast(_labellistnnz.data());
+        n = _n;
+    }
+  __device__
+  void operator()(unsigned int thread_id)
+  {
+    if(thrust::get<1>(labellist[row[row_id[thread_id]]]) == thrust::get<1>(labellist[thread_id]) && thrust::get<0>(labellist[row[row_id[thread_id]]]) != thrust::get<0>(labellist[thread_id])) {
+      atomicCAS(&(thrust::get<0>(labellist[row[row_id[thread_id]]+labellistnnz[row_id[thread_id]]])), thrust::get<0>(labellist[row[row_id[thread_id]]+labellistnnz[row_id[thread_id]]]), thrust::get<0>(labellist[thread_id]));
+      atomicCAS(&(thrust::get<1>(labellist[row[row_id[thread_id]]+labellistnnz[row_id[thread_id]]])), thrust::get<1>(labellist[row[row_id[thread_id]]+labellistnnz[row_id[thread_id]]]), thrust::get<1>(labellist[thread_id]));
+      atomicAdd(&(labellistnnz[row_id[thread_id]]), 1);
+    }
+  }
+};
+
+struct break_ties 
+{
+    int * row_id;
+    int * labellistnnz;
+    int * row;
+    int * col;
+    thrust::tuple<int,int> * labellist;
+    int n;
+    unsigned int seed;
+    break_ties(int _n,thrust::universal_vector<int> _row, thrust::universal_vector<int> _col, thrust::universal_vector<thrust::tuple<int,int> > _labellist, thrust::universal_vector<int> _row_id,
+    thrust::universal_vector<int> _labellistnnz, unsigned int _seed) {
+        row= thrust::raw_pointer_cast(&_row[0]);
+        col= thrust::raw_pointer_cast(&_col[0]);
+        labellist = thrust::raw_pointer_cast(&_labellist[0]);
+        row_id = thrust::raw_pointer_cast(&_row_id[0]);
+        labellistnnz = thrust::raw_pointer_cast(&_labellistnnz[0]);
+        n = _n;
+        seed = _seed;
+    }
+
+  __device__
+  void operator()(unsigned int thread_id)
+  {
+    //unsigned int seed = hash(thread_id);
+    thrust::default_random_engine rng(seed);
+    if(thread_id == 12)
+    printf("labellilstnnz %d\n", labellistnnz[thread_id]);
+    thrust::uniform_real_distribution<float> u01(0,labellistnnz[thread_id]);
+    if(thread_id == 12)
+    printf("random call %d\n", (int)u01(rng));
+    labellist[row[thread_id]] = labellist[row[thread_id]+(int)u01(rng)];
+  }
+};
+
+
+struct update_memory 
+{
+    int * memnnz;
+    int * row;
+    thrust::tuple<int,int> * mem;
+    thrust::tuple<int,int> * labellist;
+    int n;
+    //int * _col, int * _mem, int * _memnnz,
+    update_memory(int _n, thrust::universal_vector<int> &_row, thrust::universal_vector<thrust::tuple<int,int> > &_mem, thrust::universal_vector<thrust::tuple<int,int> > &_labellist, thrust::universal_vector<int> &_memnnz) {
+        row= thrust::raw_pointer_cast(_row.data());
+        mem = thrust::raw_pointer_cast(_mem.data());
+        labellist = thrust::raw_pointer_cast(_labellist.data());
+        memnnz = thrust::raw_pointer_cast(_memnnz.data());
+        n = _n;
+    }
+  __device__
+  void operator()(unsigned int thread_id)
+  {
+    int flag = 0;
+    for(int i = 0; i < memnnz[thread_id]; i++) {
+      if(thrust::get<0>(mem[thread_id*n+i]) == thrust::get<0>(labellist[row[thread_id]])){
+        int count = thrust::get<1>(mem[thread_id*n+i])+1;
+        thrust::tuple<int,int> val(thrust::get<0>(mem[thread_id*n+i]), count);
+        mem[thread_id*n+i] = val;
+        flag = 1;
+      }
+    }
+    if(flag == 0 && memnnz[thread_id] < n) {
+      thrust::tuple<int,int> val(thrust::get<0>(labellist[row[thread_id]]), 1);
+      mem[thread_id*n+memnnz[thread_id]] = val;
+      memnnz[thread_id]++;
+    }
+    else if(flag == 0 && memnnz[thread_id] == n) {
+      int lowcount = 1000000000;
+      int index = -1;
+      for(int i = 0; i < n; i++) {
+        if(thrust::get<1>(mem[thread_id*n+i]) < lowcount) {
+          index = i;
+          lowcount = thrust::get<1>(mem[thread_id*n+i]);
+        }
+      }
+      thrust::tuple<int,int> val(thrust::get<0>(labellist[row[thread_id]]), 1);
+      mem[thread_id*n+index] = val;
+    }
+  }
 };
 
 }
@@ -150,39 +259,238 @@ void SLPA(raft::handle_t const& handle,
                     raft::device_span<edge_t> counts,
                     bool do_expensive_check)
 {
-    //set up vectors
-    //label list vector of size # edges
-    rmm::device_uvector<edge_t> labellist(graph_view.number_of_edges(), handle.get_stream());
-    //memory vector of size n by n
-    //FIXME needs to be n by outdegree + 1 or T
-    rmm::device_uvector<vertex_t> memory(cur_graph_view.number_of_vertices() * cur_graph_view.number_of_vertices(), handle.get_stream());
-    //# of valid entries in memory vector for each vertex
-    rmm::device_uvector<vertex_t> memnnz(cur_graph_view.number_of_vertices(), handle.get_stream());
-
-
-    //Kernel 1 propagate labels by edge to all vertices
-    //currently waiting on specific primitive to better enable this
-    {
-        thrust::sequence(labellist.begin(), labellist.end());
+   int T = 50;
+    float r = 0.1;
+    int n = 13;
+    int cols = 56;
+    int mod = 1;
+    thrust::universal_vector<int> row(n+1);
+    row[0] = 0;
+    row[1] = 3;
+    row[2] = 7;
+    row[3] = 12;
+    row[4] = 17;
+    row[5] = 21;
+    row[6] = 26;
+    row[7] = 31;
+    row[8] = 34;
+    row[9] = 38;
+    row[10] = 42;
+    row[11] = 46;
+    row[12] = 49;
+    row[13] = 56;
+    thrust::universal_vector<int> col(cols);
+    col[0] = 1;
+    col[1] = 2;
+    col[2] = 3;
+    col[3] = 0;
+    col[4] = 2;
+    col[5] = 3;
+    col[6] = 12;
+    col[7] = 0;
+    col[8] = 1;
+    col[9] = 3;
+    col[10] = 6;
+    col[11] = 12;
+    col[12] = 0;
+    col[13] = 1;
+    col[14] = 2;
+    col[15] = 8;
+    col[16] = 12;
+    col[17] = 5;
+    col[18] = 6;
+    col[19] = 7;
+    col[20] = 12;
+    col[21] = 4;
+    col[22] = 6;
+    col[23] = 7;
+    col[24] = 9;
+    col[25] = 12;
+    col[26] = 2;
+    col[27] = 4;
+    col[28] = 5;
+    col[29] = 7;
+    col[30] = 12;
+    col[31] = 4;
+    col[32] = 5;
+    col[33] = 6;
+    col[34] = 3;
+    col[35] = 9;
+    col[36] = 10;
+    col[37] = 11;
+    col[38] = 5;
+    col[39] = 8;
+    col[40] = 10;
+    col[41] = 11;
+    col[42] = 8;
+    col[43] = 9;
+    col[44] = 11;
+    col[45] = 12;
+    col[46] = 8;
+    col[47] = 9;
+    col[48] = 10;
+    col[49] = 1;
+    col[50] = 2;
+    col[51] = 3;
+    col[52] = 4;
+    col[53] = 5;
+    col[54] = 6;
+    col[55] = 10;
+    thrust::universal_vector<int> row_id(cols);
+    row_id[0] = 0;    
+    row_id[1] = 0;    
+    row_id[2] = 0;    
+    row_id[3] = 1;    
+    row_id[4] = 1;    
+    row_id[5] = 1;    
+    row_id[6] = 1;
+    row_id[7] = 2;    
+    row_id[8] = 2;    
+    row_id[9] = 2;    
+    row_id[10] = 2;
+    row_id[11] = 2;
+    row_id[12] = 3;
+    row_id[13] = 3;
+    row_id[14] = 3;
+    row_id[15] = 3;
+    row_id[16] = 3;
+    row_id[17] = 4;
+    row_id[18] = 4;
+    row_id[19] = 4;
+    row_id[20] = 4;
+    row_id[21] = 5;
+    row_id[22] = 5;
+    row_id[23] = 5;
+    row_id[24] = 5;
+    row_id[25] = 5;
+    row_id[26] = 6;
+    row_id[27] = 6;
+    row_id[28] = 6;
+    row_id[29] = 6;
+    row_id[30] = 6;
+    row_id[31] = 7;
+    row_id[32] = 7;
+    row_id[33] = 7;
+    row_id[34] = 8;
+    row_id[35] = 8;
+    row_id[36] = 8;
+    row_id[37] = 8;
+    row_id[38] = 9;
+    row_id[39] = 9;
+    row_id[40] = 9;
+    row_id[41] = 9;
+    row_id[42] = 10;
+    row_id[43] = 10;
+    row_id[44] = 10;
+    row_id[45] = 10;
+    row_id[46] = 11;
+    row_id[47] = 11;
+    row_id[48] = 11;
+    row_id[49] = 12;
+    row_id[50] = 12;
+    row_id[51] = 12;
+    row_id[52] = 12;
+    row_id[53] = 12;
+    row_id[54] = 12;
+    row_id[55] = 12;
+    thrust::universal_vector<thrust::tuple<int,int> > mem(n*n);
+    thrust::universal_vector<int> memnnz(n);
+    for(int i = 0; i < n; i++) {
+        thrust::tuple<int,int> init(i, 1);
+        thrust::fill(mem.begin()+i*n, mem.begin()+(i*n+n), init);
     }
-    //Kernel 2 get the frequency of labels 
-    //assumption label list is full of values
-    {
-        //go over list and get the freqency 
-        //need a custom function to pass to transform
-        thrust::transform(labellist.begin(), labellist.end(), labellist.begin(), frequency_per_elem(labellist));
-    }
+    thrust::fill(memnnz.begin(), memnnz.end(), 1);
+    thrust::universal_vector<thrust::tuple<int,int> > labellist(cols);
+    thrust::fill(labellist.begin(), labellist.end(), 0);
 
-    //Kernel 3 labellist reduction
-    {
-        thrust::transform(labellist.begin(), labellist.end(), labellist.begin(), reduce_per_offset(labellist));
-    }
-
-    //Kernel 4 memmory label update
-    {
+    unsigned int seed;
+    for(int k = 0; k < T; k++) {
+        seed = hash(k);
+        get_label s(n, col, mem, memnnz);
+        thrust::transform(thrust::counting_iterator<int>(0), thrust::counting_iterator<int>(cols), labellist.begin(), s);
+        cudaDeviceSynchronize();
+        if(k%mod == 0) {
+            std::cout << "Speaker labellist \n";
+            for(int i = row[12]; i < row[13]; i++) {
+                std::cout << thrust::get<0>(labellist[i]) << ":" << thrust::get<1>(labellist[i]) << " ";
+            }
+            std::cout << "\n";
+        }
+        get_frequency l1(n, row, mem, labellist, row_id);
+        thrust::transform(thrust::counting_iterator<int>(0), thrust::counting_iterator<int>(cols), labellist.begin(), l1);
+        cudaDeviceSynchronize();
+        if(k%mod == 0) {
+            std::cout << "listener 1 labellist \n";
+            for(int i = row[12]; i < row[13]; i++) {
+                std::cout << thrust::get<0>(labellist[i]) << ":" << thrust::get<1>(labellist[i]) << " ";
+            }
+            std::cout << "\n";
+        }
+        sort_per_row l2(n, row, col, labellist, row_id);
+        thrust::for_each(thrust::device, thrust::counting_iterator<int>(0), thrust::counting_iterator<int>(cols), l2);
+        cudaDeviceSynchronize();
+        if(k%mod == 0) {
+           std::cout << "listener 2 labellist \n";
+            for(int i = row[12]; i < row[13]; i++) {
+                std::cout << thrust::get<0>(labellist[i]) << ":" << thrust::get<1>(labellist[i]) << " ";
+            }
+            std::cout << "\n";
+        }
+        thrust::universal_vector<int> labellistnnz(n);
+        thrust::fill(labellistnnz.begin(), labellistnnz.end(), 1);
+        check_ties l2_1(n, row, col, labellist, row_id, labellistnnz);
+        thrust::for_each(thrust::device, thrust::counting_iterator<int>(0), thrust::counting_iterator<int>(cols), l2_1);
+        cudaDeviceSynchronize();
+        if(k%mod == 0) {
+           std::cout << "listener 2_1 labellist \n";
+            for(int i = row[12]; i < row[13]; i++) {
+                std::cout << thrust::get<0>(labellist[i]) << ":" << thrust::get<1>(labellist[i]) << " ";
+            }
+            std::cout << "\n";
+        }
         
+        break_ties l2_2(n, row, col, labellist, row_id, labellistnnz, seed);
+        thrust::for_each(thrust::device, thrust::counting_iterator<int>(0), thrust::counting_iterator<int>(n), l2_2);
+        cudaDeviceSynchronize();
+        if(k%mod == 0) {
+            std::cout << "listener 2_2 labellist \n";
+            for(int i = row[12]; i < row[13]; i++) {
+                std::cout << thrust::get<0>(labellist[i]) << ":" << thrust::get<1>(labellist[i]) << " ";
+            }
+            std::cout << "\n";
+        }
+        update_memory l3(n, row, mem, labellist, memnnz);
+        thrust::for_each(thrust::device, thrust::counting_iterator<int>(0), thrust::counting_iterator<int>(n), l3);
+        cudaDeviceSynchronize();
+        if(k%mod == 0) {
+            std::cout << "Listener 3 mem\n";
+            for(int i = 0; i < n; i++) {
+                std::cout << "Vertex "<< i << ": ";
+                for(int j = 0; j < memnnz[i]; j++) {
+                    std::cout << thrust::get<0>(mem[i*n+j]) << " " << thrust::get<1>(mem[i*n+j]) << " ";
+                }
+                std::cout << "\n";
+            }
+            std::cout << "\n";
+        }
     }
-
+    post_process p(n, mem, T, r);
+    thrust::transform(thrust::counting_iterator<int>(0), thrust::counting_iterator<int>(n*n), mem.begin(), p);
+    cudaDeviceSynchronize();
+     for(int i = 0; i < n; i++) {
+        int count = 0;
+        std::cout << "Vertex " << i << " is in communities ";
+        for(int j = 0; j < memnnz[i]; j++) {
+            if(thrust::get<0>(mem[i*n+j]) != -1) {
+                std::cout << thrust::get<0>(mem[i*n+j]) << " ";
+                count++;
+            }
+        }
+        if(count > 1)
+            std::cout << "overlap\n";
+        else
+            std::cout << " only community\n";
+    }
     return;
 }
 }
